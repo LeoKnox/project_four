6dfda1c4da6723d0b754cc774abeee3224a15ae5
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-21 23:10:14 -0700

    Add files via upload

--- courses/views.py
+++ courses/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.views.genericlist import ListView
+from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Course
 
-class OwnerMixin(object, LoginRequiredMixin, PermissionRequiredMixin):
-    model = Course
-    fields = ['subject', 'title', 'slug', 'overview']
-    success_url = reverse_lazy('manage_course_list')
+class OwnerMixin(object):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
@@ -17,20 +14,20 @@ class OwnerEditMixin(object):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
-class OwerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
     success_url = reverse_lazy('manage_course_list')
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
     template_name = 'courses/manage/course/form.html'
-class ManagecourseListView(OwnerCourseMixin, Listview):
+class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
     permission_required = 'courses.view_course'
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
     permission_required = 'courses.add_course'
 class CourseUpdateview(OwnerCourseEditMixin, UpdateView):
     permission_required = 'courses.change_course'
-class CourseDeleteView(OwnercourseMixin, Deleteview):
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/maange/course/delete.html'
     permission_required = 'courses.delete_course'
 
