ecca73a37e5c727fa8c8381371a22d182a8d7998
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-24 22:54:16 -0700

    Add files via upload

--- courses/views.py
+++ courses/views.py
@@ -3,14 +3,25 @@ from django.forms.models import modelform_factory
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
 from django.apps import apps
+from django.db.models import Count
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .forms import ModuleFormSet
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('mdoules'))
+        if subject:
+            subject= get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects, 'subject':subject, 'courses': courses})
 class OwnerMixin(object):
     def get_queryset(self):
         qs = super().get_queryset()
