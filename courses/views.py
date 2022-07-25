72044d91de3c948a20f618b78a6041b243307208
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-24 23:00:49 -0700

    Add files via upload

--- courses/views.py
+++ courses/views.py
@@ -8,16 +8,20 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .forms import ModuleFormSet
 from .models import Course, Module, Content, Subject
 
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 class CourseListView(TemplateResponseMixin, View):
     model = Course
     template_name = 'courses/course/list.html'
     def get(self, request, subject=None):
         subjects = Subject.objects.annotate(total_courses=Count('courses'))
-        courses = Course.objects.annotate(total_modules=Count('mdoules'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
         if subject:
             subject= get_object_or_404(Subject, slug=subject)
             courses = courses.filter(subject=subject)
