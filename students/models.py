62deefa5bd5eac853853f457ffc8d1ed202dfd09
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-27 23:12:17 -0700

    Add files via upload

--- students/models.py
+++ students/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.template.loader import render_to_string
 
 students = models.ManyToManyField(User, related_name='courses_joined', blank=True)
+
+class ItemBase(models.Model):
+    def render(self):
+        return render_to_string(
+            f'courses/content{self._meta.mode_name}.html', {'item': self}
+        )
